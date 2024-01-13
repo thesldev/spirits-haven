5eaa8b6c82514894c0436fc9db3a1fef90ca2f96
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-01-13 09:32:03 +0530

    display user reviews and ratings of the product in display product page

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,19 +1,27 @@
 import { product } from "@/utils/product";
-import Container from "@/app/components/Container"
+import Container from "@/app/components/Container";
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRating";
 
-interface IPrams{
-    productId?: string
+interface IPrams {
+  productId?: string;
 }
 
-const Product = ({params} : {params: IPrams}) => {
-    console.log('params', params);
+const Product = ({ params }: { params: IPrams }) => {
+  console.log("params", params);
 
-    return ( <div className="p-8">
-        <Container>
-            <ProductDetails product = {product}/>
-        </Container>
-    </div> );
-}
- 
+  return (
+    <div className="p-8">
+      <Container>
+        <ProductDetails product={product} />
+        <div className="flex flex-col mt-20 gap-4">
+          <div>Add Rating</div>
+          {/* displat listed ratings */}
+          <ListRating product={product} />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export default Product;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.danmurphys.com.au", "firebasestorage.googleapis.com"],
+    domains: [
+      "media.danmurphys.com.au",
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
