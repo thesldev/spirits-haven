1684b2381cfdd8f2fe6daced1d3f466437c657c1
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-01-17 00:33:48 +0530

    create table for manage products and geta nd display data from mongodb

---
+++ app/admin/manage-products/ManageProductsClient.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { formatPrice } from "@/utils/formatPrice";
+import Heading from "@/app/components/Heading";
+
+interface ManageProductsClientProps {
+  products: Product[];
+}
+
+const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
+  products,
+}) => {
+  let rows: any = [];
+
+  if (products) {
+    rows = products.map((product) => {
+      return {
+        id: product.id,
+        name: product.name,
+        price: formatPrice(product.price),
+        category: product.category,
+        brand: product.brand,
+        inStock: product.inStock,
+        images: product.images,
+      };
+    });
+  }
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 160 },
+    { field: "name", headerName: "Name", width: 160 },
+    {
+      field: "price",
+      headerName: "Price(USD)",
+      width: 160,
+      renderCell: (params) => {
+        return (
+          <div className="font-bold text-slate-800">{params.row.price}</div>
+        );
+      },
+    },
+    { field: "category", headerName: "Category", width: 160 },
+    { field: "brand", headerName: "Brand", width: 160 },
+    {
+      field: "inStock",
+      headerName: "inStock",
+      width: 160,
+      renderCell: (params) => {
+        return (
+          <div>{params.row.inStock === true ? "in stock" : "out of stock"}</div>
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "Actions",
+      width: 220,
+      renderCell: (params) => {
+        return <div>Action</div>;
+      },
+    },
+  ];
+
+  return (
+    <div className="max-w-[1350px] m-auto text-xl">
+      <div className="mb-4 mt-8">
+        <Heading title="Manage Products" center />
+      </div>
+      <div style={{ height: 600, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 9 },
+            },
+          }}
+          pageSizeOptions={[9, 20]}
+          checkboxSelection
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ManageProductsClient;
--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,5 +1,25 @@
-const ManageProducts = () => {
-  return <div>Manage product page</div>;
+import Container from "@/app/components/Container";
+import ManageProductsClient from "./ManageProductsClient";
+import getProducts from "@/actions/getProducts";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+
+const ManageProducts = async () => {
+
+  const products = await getProducts({category: null})
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="Oops! Access denide" />;
+  }
+
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageProductsClient products = {products}/>
+      </Container>
+    </div>
+  );
 };
 
 export default ManageProducts;
