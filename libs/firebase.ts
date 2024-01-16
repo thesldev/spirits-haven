50cbf64ecb32875ecffabe885fbe0c7925e2f782
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-01-16 09:43:54 +0530

    setup firebase storage

---
+++ libs/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACPs3AzCPoAod4KggTQ7X9WsjIAjqw_dE",
+  authDomain: "spirit-have-project.firebaseapp.com",
+  projectId: "spirit-have-project",
+  storageBucket: "spirit-have-project.appspot.com",
+  messagingSenderId: "814205994991",
+  appId: "1:814205994991:web:ef9c13b0ddcf7bc799981a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
